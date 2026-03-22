9f301fd6dbe8ab41a1225a6bd4ec328a69ab00ba
olaniyi <ayodelebwj@gmail.com>
2026-03-22 15:11:27 -0400

    bjhbjhbj

--- sg.tf
+++ sg.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "private_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    security_groups = [aws_security_group.private_sg.id]
+    security_groups = [aws_security_group.public_sg.id]
   }
 
   egress {
